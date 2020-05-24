73efc634ca424a2f06fbb70878e531f51b01f782
Aurelius <aurelius.dev@gmail.com>
2020-05-24 15:39:40 +0200

    Allow downgrading of config files

--- settings.js
+++ settings.js
@@ -1,20 +1,33 @@
-"use strict"
+"use strict";
 
 const DefaultSettings = {
-    'human': false,
-    'tab': false,
-    'blacklist': []
-}
+    human: false,
+    tab: false,
+    blacklist: []
+};
 
-module.exports = function MigrateSettings(from_ver, to_ver, settings) {
-    if (from_ver === undefined) {
+module.exports = function MigrateSettings( from_ver, to_ver, settings ) {
+    if ( from_ver === undefined ) {
         // Migrate legacy config file
-        return Object.assign(Object.assign({}, DefaultSettings), settings);
-    } else if (from_ver === null) {
+        return Object.assign( Object.assign({}, DefaultSettings ), settings );
+    } else if ( from_ver === null ) {
         // No config file exists, use default settings
         return DefaultSettings;
-    } else {        
-        // Migrate from older version (using the new system) to latest one
-        throw new Error('So far there is only one settings version and this should never be reached!');
+    } else {
+        let migratedSettings = Object.assign({}, settings );
+        // Migrate from older/newer version (using the new system) to latest one
+        // upgrade...
+        for ( let cur_ver = from_ver; cur_ver < to_ver; cur_ver++ ) {
+            switch ( cur_ver ) {
+                case 1:
+                default:
+                    throw new Error( "So far there is only one settings version and this should never be reached!" );
+            }
+        }
+        if ( from_ver > to_ver ) migratedSettings = Object.assign( Object.assign({}, DefaultSettings ), settings );
+        // downgrade...
+        for ( let cur_ver = from_ver; cur_ver > to_ver; cur_ver-- );
+
+        return migratedSettings;
     }
-}
+};
